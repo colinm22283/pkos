ad5dde568fa4f2bfd95cb69856d016658ff23367
Colin McGann <colinm22@iastate.edu>
2024-05-05 15:44:33 -0500

    Updated cat command behaviour

--- source/bootloader/src/commands/cat.c
+++ source/bootloader/src/commands/cat.c
@@ -9,21 +9,16 @@
 
 #include <memory/memcpy.h>
 
-__CDECL uint32_t command_cat(uint32_t argc, const char ** argv) {
-    if (argc <= 1) {
-        console_print("Inavlid arguments\nUsage: cat <file 1> <file 2> ... <file n>\n");
-        return 1;
-    }
-
+uint32_t cat_file(const char * path) {
     directory_t directory = open_filesystem(KERNEL_LBA_START);
     if (directory == 0) return 2;
 
     uint32_t base_addr = 0;
     uint32_t i;
-    for (i = 0; argv[1][i] != '\0'; i++) {
-        if (argv[1][i] == '/') {
+    for (i = 0; path[i] != '\0'; i++) {
+        if (path[i] == '/') {
             char buffer[FILESYSTEM_NAME_MAX_SIZE];
-            memcpy(buffer, &argv[1][base_addr], i - base_addr);
+            memcpy(buffer, &path[base_addr], i - base_addr);
             buffer[i - base_addr] = '\0';
 
             directory = open_directory(directory, buffer);
@@ -34,7 +29,7 @@ __CDECL uint32_t command_cat(uint32_t argc, const char ** argv) {
     }
 
     char buffer[FILESYSTEM_NAME_MAX_SIZE];
-    memcpy(buffer, &argv[1][base_addr], i - base_addr);
+    memcpy(buffer, &path[base_addr], i - base_addr);
 
     file_t file = open_file(directory, buffer);
     if (file == 0) return 4;
@@ -46,7 +41,20 @@ __CDECL uint32_t command_cat(uint32_t argc, const char ** argv) {
     while (file_reader_read(&file_reader, c, 1)) {
         console_print(c);
     }
-    console_newline();
+
+    return 0;
+}
+
+__CDECL uint32_t command_cat(uint32_t argc, const char ** argv) {
+    if (argc <= 1) {
+        console_print("Inavlid arguments\nUsage: cat <file 1> <file 2> ... <file n>\n");
+        return 1;
+    }
+
+    for (uint32_t i = 1; i < argc; i++) {
+        uint32_t result = cat_file(argv[i]);
+        if (result != 0) return result;
+    }
 
     return 0;
 }
