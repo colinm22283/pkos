b7a5847a0b196232ca35e85b37ae715534af6b2d
Colin McGann <colinm22283@gmail.com>
2026-06-29 20:44:19 -0500

    Finished flake

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     utils.url = "github:numtide/flake-utils";
 
     pkos_binutils_flake.url = "git+ssh://git@pksrv:/git/pkos_binutils.git?rev=c3b3e890d00cdedfbfafb70b06beb428c4b195bd&shallow=1";
-    pkos_gcc_flake.url = "git+ssh://git@pksrv:/git/pkos_gcc.git?rev=a94df78a8f572758f71105915086c0e2741cda54&shallow=1";
+    pkos_gcc_flake.url = "git+ssh://git@pksrv:/git/pkos_gcc.git?rev=4d2e320e39c7effd1296f0764f34b20939680688&shallow=1";
   };
 
   outputs = { self, nixpkgs, utils, pkos_binutils_flake, pkos_gcc_flake }:
@@ -23,6 +23,8 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
+            pkgs.qemu
+
             i686Pkgs.buildPackages.gcc
             i686Pkgs.buildPackages.binutils
 
